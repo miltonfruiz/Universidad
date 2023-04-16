c93c23d8f23f084494645d3a46c9bb122c5c376f
Milton <miltonfruizok@outlook.es>
2023-04-15 23:06:37 -0300

    Ejercicio N° 9

---
+++ 1° Año/AYED/Python/01. Práctica N° 1/01. Estructura Secuencial/ej09_sec.py
@@ -0,0 +1,6 @@
+# Ejercicio N°9
+
+# Tener en cuenta, que solo se reconocen 4 operaciones básicas. ¿Cual sería el
+# algoritmo si la máquina cuenta además con la operación potencia?
+
+# Respuesta...
