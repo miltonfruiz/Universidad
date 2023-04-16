decf93ee6a645f1ca1f24a732cd83a4552d7f561
Milton <miltonfruizok@outlook.es>
2023-04-15 23:04:43 -0300

    Ejercicio N° 8

---
+++ 1° Año/AYED/Python/01. Práctica N° 1/01. Estructura Secuencial/ej08_sec.py
@@ -0,0 +1,9 @@
+# Ejercicio N°8
+
+# Dado un número, mostrar el resultado de dicho número elevado a la octava.
+
+# var numero, octava: integer;
+
+numero = int(input('Ingrese el numero: '))
+octava = numero ** 8
+print('El resultado es: ',octava)
