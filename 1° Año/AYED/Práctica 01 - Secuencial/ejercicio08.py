321de07db8ec51814e2959a4a981eb1789f3b173
Milton <miltonfruizok@outlook.es>
2023-04-15 22:28:54 -0300

    Creación carpeta práctica n°1

--- 1° Año/AYED/Práctica 01 - Secuencial/ejercicio08.py
+++
@@ -1,9 +0,0 @@
-# Ejercicio N°8
-
-# Dado un número, mostrar el resultado de dicho número elevado a la octava.
-
-# var numero, octava: integer;
-
-numero = int(input('Ingrese el numero: '))
-octava = numero ** 8
-print('El resultado es: ',octava)
--- 1° Año/AYED/Práctica 01 - Secuencial/ejercicio09.py
+++
@@ -1,6 +0,0 @@
-# Ejercicio N°9
-
-# Tener en cuenta, que solo se reconocen 4 operaciones básicas. ¿Cual sería el
-# algoritmo si la máquina cuenta además con la operación potencia?
-
-# Respuesta...
