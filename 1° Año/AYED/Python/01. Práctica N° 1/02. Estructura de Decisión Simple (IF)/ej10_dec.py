af1e88724f7d31622300f2d218e8a16015804655
Milton <miltonfruizok@outlook.es>
2023-04-16 17:52:15 -0300

    Ejercicio N° 10

---
+++ 1° Año/AYED/Python/01. Práctica N° 1/02. Estructura de Decisión Simple (IF)/ej10_dec.py
@@ -0,0 +1,21 @@
+# Ejercicio N°10
+
+# Dado como dato la cantidad de kilowatios consumidos por un usuario en un mes,
+# calcular el importe a pagar por el mismo teniendo en cuenta que:
+# Si la cantidad de kilowatios consumidos es menor ó igual a 200, el precio del kilowatio 
+# es de 0.05 pesos.
+# Si la cantidad de kilowatios consumidos es mayor que 200 y menor que 1000, el precio 
+# del kilowatio es de 0.1 pesos.
+# Si la cantidad de kilowatios consumidos es mayor ó igual que 1000, el precio del 
+# kilowatio es de 0.15 pesos.
+
+# var kilowatio: integer; precio: float;
+
+kilowatio = int(input('Ingrese la cantidad de kilowatios consumidos: '))
+if (kilowatio <= 200):
+	precio = kilowatio * 0.05
+elif (200 < kilowatio <1000):
+	precio = kilowatio * 0.01
+else:
+	precio = kilowatio * 0.15
+print('Total a pagar: ', precio,'$')
