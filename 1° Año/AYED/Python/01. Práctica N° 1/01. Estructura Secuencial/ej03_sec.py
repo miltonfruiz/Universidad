ba8dda0bdc522f89cc23f9de67b6be4ee883ed0c
Milton <miltonfruizok@outlook.es>
2023-04-15 22:52:11 -0300

    Ejercicio N° 3

---
+++ 1° Año/AYED/Python/01. Práctica N° 1/01. Estructura Secuencial/ej03_sec.py
@@ -0,0 +1,14 @@
+#Ejercicio N°3
+
+# Dados como datos cinco números obtener el promedio de los mismos e informar el
+# resultado.
+
+# var nro1, nro2, nro3, nro4, nro5: integer; promedio: float;
+
+nro1 = int(input('Ingrese el primer número: '))
+nro2 = int(input('Ingrese el segundo número: '))
+nro3 = int(input('Ingrese el tercer número: '))
+nro4 = int(input('Ingrese el cuerto número: '))
+nro5 = int(input('Ingrese el quinto número: '))
+promedio = (nro1 + nro2 + nro3 + nro4 + nro5) / 5
+print('El retultado es: ',promedio)
