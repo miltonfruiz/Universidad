9a6eb43bbc5c9322bc250a833616e7671df6e285
Milton <miltonfruizok@outlook.es>
2023-04-15 22:49:38 -0300

    Ejercicio N° 2

---
+++ 1° Año/AYED/Python/01. Práctica N° 1/01. Estructura Secuencial/ej02_sec.py
@@ -0,0 +1,13 @@
+# Ejercicio N°2
+
+# Describa lo que realiza el diagrama de Chapin. Indicar el valor que se muestra si las 
+# variables tendrían los siguientes valores: A=10, B=20, C=2
+
+# var a, b: integer; r: float;
+
+A = int(input('Ingrese el valor de A: '))
+B = int(input('Ingrese el valor de B: '))
+C = int(input('Ingrese el valor de C: '))
+R = A + B
+R = R / C
+print('El valor es: ',R)
