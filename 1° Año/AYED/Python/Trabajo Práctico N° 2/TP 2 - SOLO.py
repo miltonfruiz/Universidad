e4e0d0f4cfb49755780a5070f03ff7741d501409
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-07-13 19:30:13 -0300

    Creación módulo ACCESO + función búsqueda de usuario

--- 1° Año/AYED/Python/Trabajo Práctico N° 2/TP 2 - SOLO.py
+++ 1° Año/AYED/Python/Trabajo Práctico N° 2/TP 2 - SOLO.py
@@ -18,11 +18,112 @@ blanco = '\033[4;37m'
 reset = '\033[39m'
 cierre = '\033[0;m'
 
+#-------------------------------------------------------> Usurios de prueba <--------------------------------------------------------
+
+''' 1 admin@shopping.com       12345      administrador
+    4 localA@shopping.com      AAAA1111   dueñoLocal
+	6 localB@shopping.com      BBBB2222   dueñoLocal
+	9 unCliente@shopping.com   33xx33     cliente     '''
+
+
+#-----------------------------------------------------------> Búsquedas <------------------------------------------------------------
+
+def buscoUsuario(Usuario,Contraseña,X):
+	f = 0
+	while((X[f][1] != Usuario) and (X[f][2] != Contraseña) and (f != 3)):
+		f = f + 1
+	return f
+
+#---------------------------------------------------------> Módulo Acceso <----------------------------------------------------------
+
+def ACCESO():
+	global usuario, contraseña, f, intentos
+	print(blanco+'>>> INICIAR SESIÓN <<<'+cierre)
+	print()
+	usuario = str(input('> Ingrese usuario: '))
+	while((len(usuario) > 100) or (len(usuario) < 7)):
+		salto()
+		print(blanco+'>>> INICIAR SESIÓN <<<'+cierre)
+		print()
+		print(efc+'x ¡Usuario ingresado inválido. Mínimo 7 caracteres!'+cierre)
+		print()
+		usuario = str(input('> Ingrese usuario: '))
+	salto()
+	print(blanco+'>>> INICIAR SESIÓN <<<'+cierre)
+	print()
+	contraseña = str(input('> Ingrese contraseña: '))
+	while((len(contraseña) > 8) or (len(contraseña) < 3)):
+		salto()
+		print(blanco+'>>> INICIAR SESIÓN <<<'+cierre)
+		print()
+		print(efc+'x ¡Contraseña ingresada inválida. Mínimo 4 dígitos!'+cierre)
+		print()
+		contraseña = str(input('> Ingrese contraseña: '))
+	salto()
+	f = buscoUsuario(usuario,contraseña,D)
+	if (D[f][1] == usuario and D[f][2] == contraseña):
+		print()
+		print('Los datos son correctos!')
+		print()
+		print(pcyan+'* Presione una tecla para continuar...'+cierre)
+		msvcrt.getch()
+		salto()
+		#INGRESO(D,U)
+	else:
+		if((usuario == D[f][1]) and (contraseña != D[f][2])):
+			print()
+			print(efc+'x ¡Contraseña ingresada incorecta!'+cierre)
+			print('__________________________________')
+			print()
+			print(esc+'x Contraseña:'+cierre,ec+contraseña+cierre)
+			print()
+		elif((usuario != D[f][1]) and (contraseña == D[f][2])):
+			print()
+			print(efc+'x ¡El usuario ingresado no está registrado!'+cierre)
+			print('___________________________________________')
+			print()
+			print(esc+'x Usuario:'+cierre,ec+usuario+cierre)
+			print()
+		elif((usuario != D[f][1]) and (contraseña != D[f][2])):
+			print()
+			print(efc+'x ¡Los datos ingresados son incorrectos!'+cierre)
+			print('________________________________________')
+			print()
+			print(esc+'x Usuario:'+cierre,ec+usuario+cierre)
+			print(esc+'x Contraseña:'+cierre,ec+contraseña+cierre)
+			print()
+		intentos = intentos - 1
+		print(ec+'Le quedan',intentos,'intentos.'+cierre)
+		print()
+		print(pcyan+'* Presione una tecla para continuar...'+cierre)
+		msvcrt.getch()
+		salto()
+	if(intentos != 0):
+		print(blanco+'>>> INICIAR SESIÓN <<<'+cierre)
+		print()
+		decision = input('¿Desea continuar en el programa? S / N \n\n> ').upper()
+		print()
+		salto()
+		while (decision != 'N') and (decision != 'S'):
+			print()
+			print(blanco+'>>> INICIAR SESIÓN <<<'+cierre)
+			print()
+			print(efc+'x ¡Carácter ingresado inválido, ingrese S o N!'+cierre)
+			print()
+			decision = input('¿Desea continuar en el programa? S / N \n\n> ').upper()
+			salto()
+	else:
+		print(efc+'x !Ha alcanzado el límite de intentos!'+cierre)
+		print('______________________________________')
+		print()
+		print(pcyan+'* Presione una tecla para cerrar sesion... '+cierre)
+		msvcrt.getch()
+
+
 #-------------------------------------------------------> Programa Principal <-------------------------------------------------------
 
 def PROGRAMA():
-	users = 'admin@shopping.com'
-	password = '12345'
+	global intentos
 	intentos = 3
 	salto()
 	print(blanco+'>>> B I E N V E N I D O  <<<'+cierre)
@@ -38,84 +139,7 @@ def PROGRAMA():
 		decision = input('¿Desea iniciar sesion? S / N \n\n> ').upper()
 	salto()
 	while(decision != 'N' and intentos != 0):
-		print(blanco+'>>> INICIAR SESIÓN <<<'+cierre)
-		print()
-		userLogin = str(input('> Ingrese usuario: '))
-		while((len(userLogin) > 100) or (len(userLogin) < 7)):
-			salto()
-			print(blanco+'>>> INICIAR SESIÓN <<<'+cierre)
-			print()
-			print(efc+'x ¡Usuario ingresado inválido. Mínimo 7 caracteres!'+cierre)
-			print()
-			userLogin = str(input('> Ingrese usuario: '))
-		salto()
-		print(blanco+'>>> INICIAR SESIÓN <<<'+cierre)
-		print()
-		userPassword = str(input('> Ingrese contraseña: '))
-		while((len(userPassword) > 8) or (len(userPassword) < 3)):
-			salto()
-			print(blanco+'>>> INICIAR SESIÓN <<<'+cierre)
-			print()
-			print(efc+'x ¡Contraseña ingresada inválida. Mínimo 4 dígitos!'+cierre)
-			print()
-			userPassword = str(input('> Ingrese contraseña: '))
-		salto()
-		if(userLogin == users and userPassword == password):
-			print()
-			print('Los datos son correctos!')
-			print()
-			print(pcyan+'* Presione una tecla para continuar...'+cierre)
-			msvcrt.getch()
-			salto()
-		else:
-			if((userLogin == users) and (userPassword != password)):
-				print()
-				print(efc+'x ¡Contraseña ingresada incorecta!'+cierre)
-				print('__________________________________')
-				print()
-				print(esc+'x Contraseña:'+cierre,ec+userPassword+cierre)
-				print()
-			elif((userLogin != users) and (userPassword == password)):
-				print()
-				print(efc+'x ¡El usuario ingresado no está registrado!'+cierre)
-				print('___________________________________________')
-				print()
-				print(esc+'x Usuario:'+cierre,ec+userLogin+cierre)
-				print()
-			elif((userLogin != users) and (userPassword != password)):
-				print()
-				print(efc+'x ¡Los datos ingresados son incorrectos!'+cierre)
-				print('________________________________________')
-				print()
-				print(esc+'x Usuario:'+cierre,ec+userLogin+cierre)
-				print(esc+'x Contraseña:'+cierre,ec+userPassword+cierre)
-				print()
-			intentos = intentos - 1
-			print(ec+'Le quedan',intentos,'intentos.'+cierre)
-			print()
-			print(pcyan+'* Presione una tecla para continuar...'+cierre)
-			msvcrt.getch()
-			salto()
-		if(intentos != 0):
-			print(blanco+'>>> INICIAR SESIÓN <<<'+cierre)
-			print()
-			decision = input('¿Desea continuar en el programa? S / N \n\n> ').upper()
-			print()
-			salto()
-			while (decision != 'N') and (decision != 'S'):
-				print()
-				print(blanco+'>>> INICIAR SESIÓN <<<'+cierre)
-				print()
-				print(efc+'x ¡Carácter ingresado inválido, ingrese S o N!'+cierre)
-				print()
-				decision = input('¿Desea continuar en el programa? S / N \n\n> ').upper()
-				salto()
-		else:
-			print(efc+'x !Ha alcanzado el límite de intentos!'+cierre)
-			print('______________________________________')
-			print()
-			print(pcyan+'* Presione una tecla para cerrar sesion... '+cierre)
-			msvcrt.getch()
+		ACCESO()
 	salto()
 	print('¡Gracias por su visita!')
 	msvcrt.getch()
@@ -143,18 +167,12 @@ def cargoUsuarios():
 	global D
 	datos = [
 		['1','admin@shopping.com','12345','administrador'],
-		['2','','',''],
-		['3','','',''],
 		['4','localA@shopping.com','AAAA1111','dueñoLocal'],
-		['5','','',''],
 		['6','localB@shopping.com','BBBB2222','dueñoLocal'],
-		['7','','',''],
-		['8','','',''],
 		['9','unCliente@shopping.com','33xx33','cliente'],
-		['10','','',''],
 	  ]
-	D = ['']*10
-	for i in range(10):
+	D = ['']*4
+	for i in range(4):
 		D[i] = datos[i]
 
 def INICIO():
