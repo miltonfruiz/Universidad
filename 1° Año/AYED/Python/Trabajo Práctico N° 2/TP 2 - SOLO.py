5278d951c2c9d543c08b94c85cc76b7fb576df14
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-07-17 00:57:29 -0300

    Creación Módulos de Opciones de Administrador

--- 1° Año/AYED/Python/Trabajo Práctico N° 2/TP 2 - SOLO.py
+++ 1° Año/AYED/Python/Trabajo Práctico N° 2/TP 2 - SOLO.py
@@ -9,6 +9,8 @@ negro = '\033[30m'
 efc = '\033[3;37;41m'
 esc = '\033[3;4;31m'
 ec = '\033[3;31m'
+fan = '\033[2;7;30;43m'
+cursiva = '\033[3m'
 verde = '\033[32m'
 amarillo = '\033[4;33m'
 azul = '\033[3;4;34m'
@@ -34,10 +36,71 @@ def buscoUsuario(Usuario,Contraseña,X):
 		f = f + 1
 	return f
 
+#-----------------------------------------------> Módulos de Opciones de Administrador <---------------------------------------------
+
+def admOpcion():
+	print(blanco+'>>> MENÚ ADMINISTRADOR <<<\n'+cierre)
+	print('1. Gestión de Locales')
+	print('2. Crear cuentas de dueños de locales')
+	print('3. Aprobar / Denegar solicitud de descuento')
+	print('4. Gestión de Novedades')
+	print('5. Reporte de utilización de descuentos')
+	print('0. Salir\n')
+
+def admCartel():
+	print(cursiva+'En construcción...'+cierre)
+	print()
+	print(fan+'/ / / / / / / / / / / / / / / / / / / / /'+cierre)
+	print()
+	print(pcyan+'* Presione una tecla para volver al menú administrador...'+cierre)
+	msvcrt.getch()
+	salto()
+
+def admInvalida():
+	salto()
+	print(efc+'x Opción ingresada inválida!'+cierre)
+	print()
+	print(pcyan+'* Presione una tecla para reintentar...'+cierre)
+	msvcrt.getch()
+	salto()
+
+#------------------------------------------------------> Módulo Administrador <------------------------------------------------------
+
+def ADMINISTRADOR():
+	admOpcion()
+	opc = input('> Ingrese una opción: ')
+	while(opc != '0'):
+		match opc:
+			case '1':
+				salto()
+				print(blanco+'>>> 1. Gestión de Locales\n'+cierre)
+			case '2':
+				salto()
+				print(blanco+'>>> 2. Crear cuentas de dueños de locales\n'+cierre)
+				admCartel()
+			case '3':
+				salto()
+				print(blanco+'>>> 3. Aprobar / Denegar solicitud de descuento\n'+cierre)
+				admCartel()
+			case '4':
+				salto()
+				print(blanco+'>>> 4. Gestión de Novedades\n'+cierre)
+			case '5':
+				salto()
+				print(blanco+'>>> 5. Reporte de utilización de descuentos\n'+cierre)
+				admCartel()
+			case '0':
+				opc = '0'
+			case opc:
+				print()
+				admInvalida()
+		admOpcion()
+		opc = input('> Ingrese una opción nuevamente: ')
+
 #---------------------------------------------------------> Módulo Acceso <----------------------------------------------------------
 
 def ACCESO():
-	global usuario, contraseña, f, intentos
+	global usuario, contraseña, f, intentos, decision
 	print(blanco+'>>> INICIAR SESIÓN <<<'+cierre)
 	print()
 	usuario = str(input('> Ingrese usuario: '))
@@ -63,12 +126,15 @@ def ACCESO():
 	f = buscoUsuario(usuario,contraseña,D)
 	if (D[f][1] == usuario and D[f][2] == contraseña):
 		print()
-		print('Los datos son correctos!')
+		match D[f][3]:
+			case 'administrador':
+				ADMINISTRADOR()
+			case 'dueñoLocal':
+				print('DUEÑO LOCAL')
+			case 'cliente':
+				print('CLIENTE')
 		print()
-		print(pcyan+'* Presione una tecla para continuar...'+cierre)
-		msvcrt.getch()
 		salto()
-		#INGRESO(D,U)
 	else:
 		if((usuario == D[f][1]) and (contraseña != D[f][2])):
 			print()
@@ -123,7 +189,7 @@ def ACCESO():
 #-------------------------------------------------------> Programa Principal <-------------------------------------------------------
 
 def PROGRAMA():
-	global intentos
+	global intentos, decision
 	intentos = 3
 	salto()
 	print(blanco+'>>> B I E N V E N I D O  <<<'+cierre)
@@ -141,7 +207,7 @@ def PROGRAMA():
 	while(decision != 'N' and intentos != 0):
 		ACCESO()
 	salto()
-	print('¡Gracias por su visita!')
+	print('¡Gracias por su visita!\n')
 	msvcrt.getch()
 
 #------------------------------------------------------->  Carga de Arreglos <-------------------------------------------------------
